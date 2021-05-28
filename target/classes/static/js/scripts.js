5c3151df379d1a7347a72181bf69297afd98dd50
gleidsonguilhem <gleidson.guilhem@gmail.com>
2021-05-28 12:09:16 +1000

    Applying Thymeleaf Fragment to get header elements dynamically

--- target/classes/static/js/scripts.js
+++ target/classes/static/js/scripts.js
@@ -8,3 +8,19 @@ function callCreateAccount() {
 function callBackToLogin() {
 	window.location.href='/login';
 }
+
+
+function loginAction() {
+	/*
+	let loginBtn = document.getElementById('login')
+	
+	loginBtn.addEventListener('click', function() {
+		window.location.href="/login";
+	});
+	*/
+	window.location.href="/login";
+}
+
+function callDashBoard() {
+	window.location.href="/dashboard";
+}
